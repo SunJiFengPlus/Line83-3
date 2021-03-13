bf970d255851241031efc010884eb75eb062f259
SunJiFeng <sunjifeng@sfmail.sf-express.com>
2021-03-13 11:36:14 +0800

    删除冗余

--- src/main/java/game/Poker.java
+++ src/main/java/game/Poker.java
@@ -13,8 +13,6 @@ public class Poker {
         AbstractCard player1Card = AbstractCard.of(player1, "player1");
         AbstractCard player2Card = AbstractCard.of(player2, "player2");
 
-        String player1Type = player1Card.getType();
-        String player2Type = player2Card.getType();
         String[] type = {"StraightFlush", "FourOfAKind", "FullHouse", "Flush", "Straight", "ThreeOfAKind", "TwoPair", "OnePair", "HighCard"};
         int[] player1Number = player1Card.getCardNum();
         int[] player2Number = player2Card.getCardNum();
@@ -222,97 +220,5 @@ public class Poker {
             reResult[i] = result[result.length - i - 1];
         }
         return reResult;
-    }
-
-    private int judgeIndex(String strType) {
-        int index = -1;
-        String[] type = {"StraightFlush", "FourOfAKind", "FullHouse", "Flush", "Straight", "ThreeOfAKind", "TwoPair", "OnePair", "HighCard"};
-        for (int i = 0; i < 9; i++) {
-            if (type[i].equals(strType)) {
-                index = i;
-            }
-        }
-        return index;
-    }
-
-    private String judgeType(String str) {//判断是什么牌
-        String type;
-        String[] strArray = str.split("");
-        int[] number = strNumber(str);
-        int i;
-        String[] color = new String[5];
-        for (i = 0; i < 5; i++) {
-            color[i] = strArray[i * 3 + 1];
-        }
-        HashSet<Integer> hashSetNumber = new HashSet<>();
-        for (i = 0; i < 5; i++) {
-            hashSetNumber.add(number[i]);
-        }
-        HashSet<String> hashSetType = new HashSet<>();
-        for (i = 0; i < 5; i++) {
-            hashSetType.add(color[i]);
-        }
-        if (hashSetNumber.size() == 5) {
-            if ((number[0] - number[4] == 4) && (hashSetType.size() == 1)) {//五个相邻的数字且花色一样——同花顺
-                type = "StraightFlush";
-            } else if (number[0] - number[4] == 4 ) {//五个相邻数字——顺子
-                type = "Straight";
-            } else if (hashSetType.size() == 1) {//同一花色——同花
-                type = "Flush";
-            } else {//五个不相邻的数字——散牌
-                type = "HighCard";
-            }
-        } else if (hashSetNumber.size() == 4) {//一对相同，其余三个数字不同——对子
-            type = "OnePair";
-        } else if (hashSetNumber.size() == 3) {
-            if ((number[0] == number[1] && number[2] == number[3]) || (number[1] == number[2] && number[3] == number[4]) || (number[0] == number[1] && number[3] == number[4])) {//两对
-                type = "TwoPair";
-            } else {//三个数字相同，另外两个数字不同——三条
-                type = "ThreeOfAKind";
-            }
-        } else {
-            if (number[0] != number[1] || number[3] != number[4]) {//四个数字相同——铁支
-                type = "FourOfAKind";
-            } else {//三个数字相同，另外两个数字相同——葫芦
-                type = "FullHouse";
-            }
-        }
-        return type;
-    }
-
-    private int[] strNumber(String str) {//数字转化并将其从大到小排序
-        int[] number = new int[5];
-        String[] strArray = str.split("");
-        int i;
-        for (i = 0; i < 5; i++) {
-            String c = strArray[i * 3];
-            switch (c) {
-                case "T":
-                    number[i] = 10;
-                    break;
-                case "J":
-                    number[i] = 11;
-                    break;
-                case "Q":
-                    number[i] = 12;
-                    break;
-                case "K":
-                    number[i] = 13;
-                    break;
-                case "A":
-                    number[i] = 14;
-                    break;
-                default:
-                    number[i] = Integer.parseInt(c);
-                    break;
-            }
-        }
-
-        Arrays.sort(number);
-        int[] renumber = new int[number.length];
-        for (i = 0; i < number.length; i++) {
-            renumber[i] = number[number.length - i - 1];
-        }
-        return renumber;
     }
 }
